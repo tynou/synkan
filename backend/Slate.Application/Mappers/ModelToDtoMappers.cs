00e6d5b1688148d33fd97c99cc740f6800973e68
Alexander Timonin <2sdaiwxet@gmail.com>
2026-06-22 20:19:12 +0500

    feat: board dto mapper

---
+++ backend/Slate.Application/Mappers/ModelToDtoMappers.cs
@@ -0,0 +1,22 @@
+﻿using Slate.Application.Dto.Response;
+using Slate.Domain.Entities;
+
+namespace Slate.Application.Mappers;
+
+public static class ModelToDtoMappers
+{
+    public static BoardDto ToDto(this Board board)
+    {
+        return new BoardDto(
+            board.Id,
+            board.OwnerId,
+            board.Title,
+            board.Columns.Select(c => c.ToDto())
+        );
+    }
+
+    public static ColumnDto ToDto(this Column column)
+    {
+        return new ColumnDto();
+    }
+}
