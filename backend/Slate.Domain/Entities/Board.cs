47056353b567037e7f47f9d1c9cad9eac8179c66
Alexander Timonin <2sdaiwxet@gmail.com>
2026-06-22 20:18:28 +0500

    feat: add owner id to board

--- backend/Slate.Domain/Entities/Board.cs
+++ backend/Slate.Domain/Entities/Board.cs
@@ -3,14 +3,16 @@
 public class Board
 {
     public Guid Id { get; private set; }
+    public Guid OwnerId { get; private set; }
     public string Title { get; private set; }
 
     private readonly List<Column> _columns = [];
     public IReadOnlyCollection<Column> Columns => _columns;
     
-    public  Board(Guid id, string title)
+    public  Board(Guid id, Guid ownerId, string title)
     {
         Id = id;
+        OwnerId = ownerId;
         Title = title;
     }
 }
