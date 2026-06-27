41b5130e04431662038d4de7a9bef1314238e443
Alexander Timonin <2sdaiwxet@gmail.com>
2026-06-27 20:35:00 +0500

    feat: add board member table

--- backend/Slate.Infrastructure/Persistence/AppDbContext.cs
+++ backend/Slate.Infrastructure/Persistence/AppDbContext.cs
@@ -9,6 +9,7 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
     public DbSet<Board> Boards { get; set; }
     public DbSet<Column> Columns { get; set; }
     public DbSet<Card> Cards { get; set; }
+    public DbSet<BoardMember> BoardMembers { get; set; }
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
