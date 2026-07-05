335184a4bd3cd0791baef059b8bf03d4198acecc
Alexander Timonin <2sdaiwxet@gmail.com>
2026-07-05 20:27:30 +0500

    feat: chat message configuration

---
+++ backend/Slate.Infrastructure/Persistence/Configuration/ChatMessageConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Slate.Domain.Entities;
+
+namespace Slate.Infrastructure.Persistence.Configuration;
+
+public class ChatMessageConfiguration : IEntityTypeConfiguration<ChatMessage>
+{
+    public void Configure(EntityTypeBuilder<ChatMessage> builder)
+    {
+        builder.HasKey(cm => cm.Id);
+        
+        builder.Property(m => m.CreatedAt)
+            .ValueGeneratedOnAdd()
+            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+        
+        builder.HasOne<Board>()
+            .WithMany()
+            .HasForeignKey(cm => cm.BoardId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
