463d4fbff74e5c9a54b22d6d17d0e7946cdcbeed
Alexander Timonin <2sdaiwxet@gmail.com>
2026-07-05 20:27:44 +0500

    feat: chat message repository

---
+++ backend/Slate.Domain/Repositories/IChatMessageRepository.cs
@@ -0,0 +1,10 @@
+﻿using Slate.Domain.Entities;
+
+namespace Slate.Domain.Repositories;
+
+public interface IChatMessageRepository
+{
+    Task AddAsync(ChatMessage message);
+
+    Task<IReadOnlyCollection<ChatMessage>> GetByBoardIdAsync(Guid boardId);
+}
